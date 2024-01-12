31f06bd6900d0475d81d62217d39e10ec39afdc6
tammorderweb <ordertamm@gmail.com>
2024-01-12 21:46:44 +0300

    Initial commit

---
+++ packages/syncfusion_flutter_core/lib/tooltip_internal.dart
@@ -0,0 +1,10 @@
+library tooltip_internal;
+
+import 'dart:async';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+// ignore: depend_on_referenced_packages
+import 'core.dart';
+part 'src/tooltip/tooltip.dart';
+part 'src/test/tooltip_tests.dart';
